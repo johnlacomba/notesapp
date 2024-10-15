46afb0e507c35670e13f159a02b9c0b94bf4d5bf
Ubuntu <ubuntu@ip-172-31-81-4.ec2.internal>
2024-10-15 19:02:51 +0000

    Updating App.jsx with example chessboard code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Chess from "chess.js";
+import { Chessboard } from "react-chessboard";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function PlayRandomMoveEngine() {
+	  const [game, setGame] = useState(new Chess());
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	  function makeAMove(move) {
+		      const gameCopy = { ...game };
+		      const result = gameCopy.move(move);
+		      setGame(gameCopy);
+		      return result; // null if the move was illegal, the move object if the move was legal
+		    }
+
+	  function makeRandomMove() {
+		      const possibleMoves = game.moves();
+		      if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+			        return; // exit if the game is over
+		      const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+		      makeAMove(possibleMoves[randomIndex]);
+		    }
+
+	  function onDrop(sourceSquare, targetSquare) {
+		      const move = makeAMove({
+			            from: sourceSquare,
+			            to: targetSquare,
+			            promotion: "q", // always promote to a queen for example simplicity
+			          });
+
+		      // illegal move
+		      if (move === null) return false;
+		      setTimeout(makeRandomMove, 200);
+		      return true;
+		    }
+
+	  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
 }
-
-export default App
