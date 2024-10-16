cd3c564986c4369c36af465fa88d89623e9eb3fb
johnlacomba <64287227+johnlacomba@users.noreply.github.com>
2024-10-16 16:37:48 -0400

    Update main.jsx with original version

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-        <App />
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
