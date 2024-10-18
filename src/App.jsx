2951b6d1f96865a81b227f5f83291e33e640ca04
johnlacomba <64287227+johnlacomba@users.noreply.github.com>
2024-10-18 16:12:04 -0400

    Update App.jsx with code for random moves from the opposing side
    https://react-chessboard.vercel.app/?path=/story/chessboard--play-vs-random

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,65 @@ import { Chessboard } from "react-chessboard";
 
 export default function Board() {
   const [game, setGame] = useState(new Chess());
-
-  const makeMove = (move) => {
-    game.move(move)
-    setGame(new Chess(game.fen()))
+  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout>();
+  function safeGameMutate(modify) {
+    setGame(g => {
+      const update = {
+        ...g
+      };
+      modify(update);
+      return update;
+    });
   }
-
   function makeRandomMove() {
     const possibleMoves = game.moves();
-    if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0) return; // exit if the game is over
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    makeMove(possibleMoves[randomIndex]);
-  }
 
-  function onDrop(sourceSquare, targetSquare) {
-    const move = makeMove({
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return;
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    safeGameMutate(game => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+  function onDrop(sourceSquare, targetSquare, piece) {
+    const gameCopy = {
+      ...game
+    };
+    const move = gameCopy.move({
       from: sourceSquare,
       to: targetSquare,
-      promotion: "q", // always promote to a queen for example simplicity
+      promotion: piece[1].toLowerCase() ?? "q"
     });
+    setGame(gameCopy);
 
     // illegal move
     if (move === null) return false;
-    setTimeout(makeRandomMove, 200);
+
+    // store timeout so it can be cleared on undo/reset so computer doesn't execute move
+    const newTimeout = setTimeout(makeRandomMove, 200);
+    setCurrentTimeout(newTimeout);
     return true;
   }
-
-  return(
-     <div>
-        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
-     </div>
-  )
-}
+  return <div style={boardWrapper}>
+      <Chessboard id="PlayVsRandom" position={game.fen()} onPieceDrop={onDrop} customBoardStyle={{
+      borderRadius: "4px",
+      boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)"
+    }} />
+      <button style={buttonStyle} onClick={() => {
+      safeGameMutate(game => {
+        game.reset();
+      });
+      clearTimeout(currentTimeout);
+    }}>
+        reset
+      </button>
+      <button style={buttonStyle} onClick={() => {
+      safeGameMutate(game => {
+        game.undo();
+      });
+      clearTimeout(currentTimeout);
+    }}>
+        undo
+      </button>
+    </div>;
+  }
