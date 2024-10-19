1edf58e87ed6632c52c802b78a494a1aba08281a
johnlacomba <64287227+johnlacomba@users.noreply.github.com>
2024-10-19 13:21:11 -0400

    Update App.jsx with a refactor to fix the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import React, { forwardRef, useEffect, useRef, useState, useMemo } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
+const buttonStyle = {
+  cursor: "pointer",
+  padding: "10px 20px",
+  margin: "10px 10px 0px 0px",
+  borderRadius: "6px",
+  backgroundColor: "#f0d9b5",
+  border: "none",
+  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.5)",
+};
+
 export default function Board() {
   const [game, setGame] = useState(new Chess());
-
-  const buttonStyle = {
-    cursor: "pointer",
-    padding: "10px 20px",
-    margin: "10px 10px 0px 0px",
-    borderRadius: "6px",
-    backgroundColor: "#f0d9b5",
-    border: "none",
-    boxShadow: "0 2px 5px rgba(0, 0, 0, 0.5)",
-  };
+  const [currentTimeout, setCurrentTimeout] = useState<NodeJS.Timeout>();
 
   function safeGameMutate(modify) {
     setGame((g) => {
@@ -22,52 +23,71 @@ export default function Board() {
       return update;
     });
   }
-  
-  const makeMove = (move) => {
-    game.move(move)
-    setGame(new Chess(game.fen()))
-  }
 
   function makeRandomMove() {
     const possibleMoves = game.moves();
-    if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0) return; // exit if the game is over
+
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return;
+
     const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    makeMove(possibleMoves[randomIndex]);
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
   }
 
-  function onDrop(sourceSquare, targetSquare) {
-    const move = makeMove({
+  function onDrop(sourceSquare, targetSquare, piece) {
+    const gameCopy = { ...game };
+    const move = gameCopy.move({
       from: sourceSquare,
       to: targetSquare,
-      promotion: "q", // always promote to a queen for example simplicity
+      promotion: piece[1].toLowerCase() ?? "q",
     });
+    setGame(gameCopy);
 
     // illegal move
     if (move === null) return false;
-    setTimeout(makeRandomMove, 200);
+
+    // store timeout so it can be cleared on undo/reset so computer doesn't execute move
+    const newTimeout = setTimeout(makeRandomMove, 200);
+    setCurrentTimeout(newTimeout);
     return true;
   }
 
-  return(
-     <div>
-       <Chessboard position={game.fen()} onPieceDrop={onDrop} customBoardStyle={{
-       borderRadius: "4px",
-       boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)"
-    }} />
-       <button style={buttonStyle} onClick={() => {
-       safeGameMutate(game => {
-         game.reset();
-       });
-    }}>
-         reset
-       </button>
-       <button style={buttonStyle} onClick={() => {
-       safeGameMutate(game => {
-         game.undo();
-       });
-    }}>
-         undo
-       </button>
-     </div>
-  )
-}
+  return (
+    <div style={boardWrapper}>
+      <Chessboard
+        id="PlayVsRandom"
+        position={game.fen()}
+        onPieceDrop={onDrop}
+        customBoardStyle={{
+          borderRadius: "4px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+        }}
+      />
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.reset();
+          });
+          clearTimeout(currentTimeout);
+        }}
+      >
+        reset
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          safeGameMutate((game) => {
+            game.undo();
+          });
+          clearTimeout(currentTimeout);
+        }}
+      >
+        undo
+      </button>
+    </div>
+  );
+};
